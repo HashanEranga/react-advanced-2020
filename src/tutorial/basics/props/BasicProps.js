7892480b2b6691bef79b8fc9bbca17533ae03436
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-13 23:33:56 +0530

    create(): book list using props

--- src/tutorial/basics/props/BasicProps.js
+++ src/tutorial/basics/props/BasicProps.js
@@ -1,51 +1,68 @@
 import React from 'react'
 
-// variables
-const title = "Title of the book"
-const author = "Name of the Author"
-const image = "https://www.bigw.com.au/medias/sys_master/images/images/hc1/h28/29455282241566.jpg" 
-const width = '200'
-const height = '200'
-
 // book shelf
 function BasicProps() {
     return (
-        <section>
-            <Book
-                title = {title}
-                author = {author}
-                image = {image}
-            />
-            <Book
-                title = {title}
-                author = {author}
-                image = {image}
-            />
-            <Book
-                title = {title}
-                author = {author}
-                image = {image}
-            />
-        </section>
+    <section>
+        <BookList/>
+    </section>
     )
 }
 
+function BookList() {
+    return(
+        <>
+            {books.map((book) =>{
+                return (
+                    <Book
+                        image = {book.img}
+                        author = {book.author}
+                        title = {book.title}
+                    />
+                )
+            })}
+        </>
+    )
+}
 // Book Component
 // direct method to define attributes instead of props
-function Book({image, title, author}) {
+function Book(props) {
     // content of the book
 
     // another method of defining props attributes
-    // const {image, author, title} = props
+    const {image, author, title} = props
 
     return(
         <article>
-            <img src={image} alt='' width={width} height={height}/>
+            <img src={image} alt='' width='200' height='200'/>
             <h4>{author}</h4>
             <h4>{title}</h4>
         </article>
     )
 }
 
+// Book List
+const books = [
+    {
+        img: 'https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg',
+        arthor: 'Will Smith',
+        title: 'Will'
+    },
+    {
+        img: 'https://m.media-amazon.com/images/I/51SN8LtEkGL.jpg',
+        arthor: 'Jamie Foxx (Author, Narrator), Corinne Foxx (Narrator), Grand Central Publishing (Publisher)',
+        title: 'Act Like You Got Some Sense: And Other Things My Daughters Taught Me'
+    },
+    {
+        img: 'https://images-na.ssl-images-amazon.com/images/I/911cmGSgcvL._AC_UL254_SR254,254_.jpg',
+        arthor: 'Bernie Mac (Author, Narrator), HarperAudio (Publisher)',
+        title: 'Maybe You Never Cry Again'
+    },
+    {
+        img: 'https://m.media-amazon.com/images/I/41JyuuGIctL.jpg',
+        arthor: 'Paul Mooney (Author), Dave Chappelle (Author), Bobby Spears Jr. (Narrator), Urban AudioBooks (Publisher)',
+        title: 'Black Is the New White'
+    }
+]
 
 export default BasicProps
