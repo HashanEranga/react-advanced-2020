2f7c90517df25971a6723a14e505071e3480ecb1
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-14 08:41:23 +0530

    create(): use state array example

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
 // import Greetings from './tutorial/basics/firstComponent/FirstComponent'
 // import BasicProps from './tutorial/basics/props/BasicProps'
 // import BookList from './tutorial/basics/props/BookList'
-import Setup from './tutorial/1-useState/setup/2-useState-basics'
+import Setup from './tutorial/1-useState/setup/3-useState-array'
 function App() {
   return (
     <div className="container">
